1f26bf691d63c398bd377938453b78093198ef5f
Mike Johnson <37885961+mikeyj125@users.noreply.github.com>
2019-05-02 12:19:47 -0500

    DONT USE

---
+++ CyborgGame/Assets/ForExport/Scripts/DoorOpenFirst.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorOpenFirst : MonoBehaviour
+{
+    public GameObject theDoor;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        theDoor.GetComponent<Animator>().Play("DoorOpen");
+    }
+
+}
---
+++ CyborgGame/Assets/ForExport/Scripts/DoorOpenSecond.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorOpenSecond : MonoBehaviour
+{
+    public GameObject theDoor1;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        theDoor1.GetComponent<Animator>().Play("DoorOpen1");
+    }
+
+}
